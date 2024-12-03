c64726e1c9b94f0e3ef1b24cee1fdfa3e56d5163
Tristan Yang <yanggc888@163.com>
2024-12-03 14:59:45 +0800

    chore: updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pkg from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: (new URL(pkg.homepage)).pathname,
+  base: '/',
   server: {
     port: 3006
   },
